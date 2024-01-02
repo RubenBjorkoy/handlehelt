be01efdaa08de318c22b7577c8dc8053b66de289
Ruben Vega Bjørkøy <rubenvb@stud.ntnu.no>
2024-01-02 10:57:44 +0100

    Secret may be working?

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
+// Fetch and parse the .env file
+fetch('secrets.env')
+  .then((response) => response.text())
+  .then((envContent) => {
+    const envLines = envContent.split('\n');
+    for (const line of envLines) {
+      const [key, value] = line.split('=');
+      if (key && value) {
+        // Set environment variables in the browser's global scope (window)
+        window[key.trim()] = value.trim();
+      }
+    }
 
+    // Now, you can access API_KEY like this:
+    const API_KEY = window.API_KEY;
 // Initialize Firebase and fetch data after the SDK is fully loaded
-function initializeFirebaseAndFetchData() {
-  const firebaseConfig = {
-    apiKey: API_KEY,
-    authDomain: "handlehelt.firebaseapp.com",
-    projectId: "handlehelt",
-    storageBucket: "handlehelt.appspot.com",
-    messagingSenderId: "1005392887603",
-    appId: "1:1005392887603:web:7c70a6470b3738aac38846",
-    databaseURL: "https://handlehelt-default-rtdb.europe-west1.firebasedatabase.app"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  // Reference to the database
-  const database = firebase.database();
-  const dbRef = database.ref('/butikker/kiwi/produkter/kategorier');
-  // Fetch data
-  dbRef.once('value')
-    .then((snapshot) => {
-      const data = snapshot.val();
-      console.log('Data from Firebase Realtime Database:', data);
-    })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
-    });
-}
-
-window.addEventListener('load', initializeFirebaseAndFetchData);
+    function initializeFirebaseAndFetchData() {
+      const firebaseConfig = {
+        apiKey: API_KEY,
+        authDomain: "handlehelt.firebaseapp.com",
+        projectId: "handlehelt",
+        storageBucket: "handlehelt.appspot.com",
+        messagingSenderId: "1005392887603",
+        appId: "1:1005392887603:web:7c70a6470b3738aac38846",
+        databaseURL: "https://handlehelt-default-rtdb.europe-west1.firebasedatabase.app"
+      };
+      
+      firebase.initializeApp(firebaseConfig);
+      // Reference to the database
+      const database = firebase.database();
+      const dbRef = database.ref('/butikker/kiwi/produkter/kategorier');
+      // Fetch data
+      dbRef.once('value')
+        .then((snapshot) => {
+          const data = snapshot.val();
+          console.log('Data from Firebase Realtime Database:', data);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    }
+    initializeFirebaseAndFetchData();
+});
