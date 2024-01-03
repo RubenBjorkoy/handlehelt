7353437e66eadbdfe3709eb35ab4abe2fa0d2df1
Ruben Vega Bjørkøy <rubenvb@stud.ntnu.no>
2024-01-03 18:08:47 +0100

    React development environment set up

---
+++ src/components/homepage.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Component } from "react-simplified";
+import { Card, Row, Column, Form, Button } from "../widgets.tsx";
+import { createHashHistory } from "history";
+import rdbService from '../services/RDB-service.js';
+
+const history = createHashHistory();
+
+class HomePage extends Component {
+
+    async mounted() {
+        try {
+            const data = await rdbService.fetchRDB();
+            console.log('Fetched data:', data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+        return 0;
+    }
+    render() {
+        return (
+            <div className="container">
+                <Card title="Coming Soon">
+                </Card>
+            </div>
+        );
+    }
+
+    
+}
+
+export default HomePage;
