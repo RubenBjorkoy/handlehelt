540d3ffa89dd65f78ddecbc8b15f2070e60c3a3b
Halvor Njåstad <halvorin@uio.no>
2024-01-06 17:14:39 +0100

    comments

--- src/components/classes/SundayStore.ts
+++ src/components/classes/SundayStore.ts
@@ -3,5 +3,5 @@ class SundayStore extends Store {
   //reason this may be usefull:
   //if we spesifically want to find only sunday stores,
   //we just search based on class, not on an arbitrary time-object inside each store-object. cleaner code.
-  //this means the class serves a purpose, even though it is seemingly empty.
+  //this means the class serves a purpose, even though it is seemingly empty. Polymorphism, bitch!
 }
